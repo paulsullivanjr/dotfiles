df0358443e66f8a68a5caf0f10034caf3232ff6a
Paul Sullivan <psullivan@thoroughcare.net>
2026-05-01 21:41:33 -0400

    nvim update

---
+++ nvim/lua/plugins/elixir.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        elixirls = false,
+        lexical = false,
+        nextls = false,
+        expert = {},
+      },
+    },
+  },
+}
