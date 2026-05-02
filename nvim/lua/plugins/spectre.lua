86e6cca26ed8f53649191504c073a0673b91e214
paulsullivanjr <paulsullivan1@gmail.com>
2026-05-01 21:44:59 -0400

    clean up

---
+++ nvim/lua/plugins/spectre.lua
@@ -0,0 +1,38 @@
+return {
+  "nvim-pack/nvim-spectre",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+    {
+      "<leader>SS",
+      function()
+        require("spectre").toggle()
+      end,
+      desc = "Toggle Spectre",
+    },
+    {
+      "<leader>sw",
+      function()
+        require("spectre").open_visual({ select_word = true })
+      end,
+      desc = "Search current word",
+    },
+    {
+      "<leader>sw",
+      function()
+        require("spectre").open_visual()
+      end,
+      mode = "v",
+      desc = "Search current word",
+    },
+    {
+      "<leader>sp",
+      function()
+        require("spectre").open_file_search({ select_word = true })
+      end,
+      desc = "Search on current file",
+    },
+  },
+  config = function()
+    require("spectre").setup()
+  end,
+}
